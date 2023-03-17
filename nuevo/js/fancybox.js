73e91b24e319c1daf7a3c54c2051b0e2e6b3815d
Jose Perez <55522084+aladuuu@users.noreply.github.com>
2023-03-17 17:20:37 -0300

    update

--- nuevo/js/fancybox.js
+++ nuevo/js/fancybox.js
@@ -1,3 +1,20 @@
 Fancybox.bind("[data-fancybox]", {
     closeButton: false,
+    Thumbs: {
+        minCount: 3,
+        type: "classic",
+        showOnStart: false,
+    },
+    Toolbar: {
+        display: {
+            left: [],
+            middle: [],
+            right: [
+              "iterateZoom",
+              "fullscreen",
+              "thumbs",
+              "close",
+            ],
+        },
+    },   
 });
